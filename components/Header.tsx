fd89e7b0abe4618b96e4d2630433ddd7f1461c64
Bongani Masango <bkmasango26@gmail.com>
2023-05-03 16:00:24 +0200

    commit

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <div>
+        <div className="container mx-auto"></div>
+    </div>
+  )
+}
+
+export default Header
